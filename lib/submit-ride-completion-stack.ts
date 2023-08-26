4d915e223d15bf1a720ad4eb043eabd0d2c24bca
Bruno Carpio <bcarpio181@gmail.com>
2023-08-26 17:41:04 -0500

    Update lambda function to use typescript

--- lib/submit-ride-completion-stack.ts
+++ lib/submit-ride-completion-stack.ts
@@ -1,26 +1,29 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Construct } from 'constructs';
-import * as apigwv2 from '@aws-cdk/aws-apigatewayv2-alpha';
-import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
-export class SubmitRideCompletionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class SubmitRideCompletionStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const managementFunction = new lambda.Function(this, 'Rides-Processor', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'rides-processor.handler',
-      code: lambda.Code.fromAsset('functions')
+    const managementFunction = new Function(this, "Rides-Processor", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "rides-processor.handler",
+      code: Code.fromAsset("functions"),
     });
 
-    const ridesIntegration = new HttpLambdaIntegration('Rides-Management-Integration', managementFunction);
+    const ridesIntegration = new HttpLambdaIntegration(
+      "Rides-Management-Integration",
+      managementFunction
+    );
 
-    const httpApi = new apigwv2.HttpApi(this, 'Rides-Management-Http-Api');
+    const httpApi = new HttpApi(this, "Rides-Management-Http-Api");
     httpApi.addRoutes({
-      path: '/submit/rides/create',
-      methods: [ apigwv2.HttpMethod.POST ],
-      integration: ridesIntegration
-    })
+      path: "/submit/rides/create",
+      methods: [HttpMethod.POST],
+      integration: ridesIntegration,
+    });
   }
 }
