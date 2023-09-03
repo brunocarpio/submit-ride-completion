6061d30f1c1e1e49c9e5e2b01402bb4aa721142b
Bruno Carpio <bruno.carpio@contractors.lesschwab.com>
2023-09-03 17:01:15 -0300

    Remove lambda and rest api

--- lib/submit-ride-completion-stack.ts
+++ lib/submit-ride-completion-stack.ts
@@ -1,10 +1,6 @@
-import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
-import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Topic } from "aws-cdk-lib/aws-sns";
+import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Queue } from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 
@@ -17,31 +13,5 @@ export class SubmitRideCompletionStack extends Stack {
     const queue = new Queue(this, "Test-Queue");
 
     topic.addSubscription(new SqsSubscription(queue));
-
-    const lambda = new NodejsFunction(this, "Rides-Processor", {
-      runtime: Runtime.NODEJS_18_X,
-      entry: "./functions/rides-processor.ts",
-      bundling: {
-        minify: true,
-        sourceMap: true,
-      },
-      environment: {
-        SNS_TOPIC_ARN: topic.topicArn,
-      },
-    });
-
-    topic.grantPublish(lambda);
-
-    const lambdaIntegration = new HttpLambdaIntegration(
-      "Rides-Management-Integration",
-      lambda
-    );
-
-    const httpApi = new HttpApi(this, "Rides-Management-Http-Api");
-    httpApi.addRoutes({
-      path: "/submit/ride/create",
-      methods: [HttpMethod.POST],
-      integration: lambdaIntegration,
-    });
   }
 }
